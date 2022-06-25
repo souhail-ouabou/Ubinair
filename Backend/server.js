fd7beeadac0a45943e5309342e885e53a3682296
unknown <souhail7ouabou@gmail.com>
2022-06-25 20:36:09 +0100

    prepare for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -25,5 +25,32 @@ app.use('/user', require('./routes/userRouter'))
 app.use('/projets', require('./routes/projetRouter'))
 app.use("/api", require("./routes/upload"));
 
+__dirname = path.resolve();
+console.log(__dirname);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Runn....");
+  });
+}
+
+__dirname = path.resolve();
+console.log(__dirname);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Runn....");
+  });
+}
+
+
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
